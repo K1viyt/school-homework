04473ddc47114735590449b5cd025041efe2d2ce
K1viyt <vlad.hapilin@gmail.com>
2026-02-27 19:43:17 +0300

    feat: добавить загрузку файлов

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/K1viyt/school-homework/internal/handlers"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Сервер работает!")
+		w.Write([]byte("Сервер работает!"))
 	})
+	http.HandleFunc("/upload", handlers.UploadHandler)
 
 	log.Println("Сервер запущен на http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,39 @@
- 
+package handlers
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Разрешён только POST", http.StatusMethodNotAllowed)
+		return // ← без этого код ниже выполнится!
+	}
+	file, h, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, "Ошибка при получении файла", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	f, err := os.Create(filepath.Join("uploads", h.Filename))
+	if err != nil {
+		http.Error(w, "Не удалось сохранить файл", http.StatusInternalServerError)
+		log.Println("Ошибка создания файла:", err)
+		return
+	}
+	defer f.Close()
+	_, err = io.Copy(f, file)
+	if err != nil {
+		http.Error(w, "Ошибка при получении файла", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(w, "файл %s загружен", h.Filename)
+
+}
